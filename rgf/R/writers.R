7be7edaa3c46d2592ccc9c60f649734074f6c2a7
Zach Mayer <zach.mayer@gmail.com>
2013-11-07 13:22:36 -0500

    Cleaned up the rgf writer code and added documentation

--- rgf/R/writers.R
+++ rgf/R/writers.R
@@ -1,25 +1,31 @@
-require(caret)
-require(e1071)
-require(Matrix)
-require(SparseM)
-require(boot)
-
-write.RGF.X.file <- function(X, file){
-
+#' Write the RGF X matrix to a file
+#' 
+#' This function writes a data.frame to the RGF file format
+#' 
+#' @param X A data.frame
+#' @param finalTrainFile the file to write the matrix to
+#' @export
+#' @return The path of the file
+#' @examples 
+#' data(iris)
+#' f <- write.RGF.X.file(head(iris))
+#' cat(readChar(f, file.info(f)$size))
+#' unlink(f)
+write.RGF.X.file <- function(X, finalTrainFile=tempfile()){
+  require('caret')
+  require('e1071')
+  
 	tempFile1 <- tempfile()
-	tempFile2 <- tempfile()
-	finalTrainFile <- tempfile()
 
 	dv1 <- dummyVars( ~., data = X, sparse = T)
 	X2 <- as.matrix.csr(predict(dv1, X))
 	write.matrix.csr( as(predict(dv1, X), "matrix.csr"), tempFile1 ,  fac = TRUE)
 	
-	paste('sparse ',max(X2@ja)+1 ,sep = '')
 	header1 <- paste('sparse ',max(X2@ja)+1,sep = '')
 
 	temp1 <- as.vector(readLines(con = tempFile1))
-
-	temp2 <- c(header1, temp1)
-	write(temp2, finalTrainFile )
-	
+	write(c(header1, temp1), finalTrainFile)
+  
+  unlink(tempFile1)
+  return(finalTrainFile)
 }
